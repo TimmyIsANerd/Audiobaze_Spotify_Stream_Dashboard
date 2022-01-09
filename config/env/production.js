d629b947eebd17b530805f8ca00571e2dbe61fb7
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-01-09 22:38:28 +0100

    fixed prod

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,11 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'https://example.com',
+      'https://staging.example.com',
+      'gracious-lichterman-926b23.netlify.app'
+    ],
 
 
     /***************************************************************************
