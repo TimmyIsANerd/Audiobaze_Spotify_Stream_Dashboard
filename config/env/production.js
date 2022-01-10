b08f45279b3734e5c3a080e4cacf2fe81aac0d1c
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-01-11 00:57:08 +0100

    reconfigured sessions

--- config/env/production.js
+++ config/env/production.js
@@ -185,11 +185,17 @@ module.exports = {
     * > (For a full list, see https://sailsjs.com/plugins/sessions)            *
     *                                                                          *
     ***************************************************************************/
-    adapter: 'connect-mongodb-session',
-    uri: 'mongodb+srv://testUser:TheTitan1123@audiobaze.8pz2s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 10000,
+    adapter: 'connect-mongo',
+    url: 'mongodb+srv://testUser:TheTitan1123@audiobaze.8pz2s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    collection: 'sessions',
+    stringify: true,
+    auto_reconnect: false,
+    mongoOptions: {
+      server: {
+        ssl: true
+      }
+    },
+
     //--------------------------------------------------------------------------
     // /\   OR, to avoid checking it in to version control, you might opt to
     // ||   set sensitive credentials like this using an environment variable.
