11ee27d4c2ca5d1a665550443b9a5e3f988e0a5f
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-01-29 02:15:44 +0100

    changed verifyemail status

--- config/custom.js
+++ config/custom.js
@@ -68,18 +68,18 @@ module.exports.custom = {
   //--------------------------------------------------------------------------
 
   // The sender that all outgoing emails will appear to come from.
-  fromEmailAddress: 'noreply@example.com',
+  fromEmailAddress: 'audiobazze@gmail.com',
   fromName: 'The Audiobaze Team',
 
   // Email address for receiving support messages & other correspondences.
   // > If you're using the default privacy policy, this will be referenced
   // > as the contact email of your "data protection officer" for the purpose
   // > of compliance with regulations such as GDPR.
-  internalEmailAddress: 'support+development@example.com',
+  internalEmailAddress: 'audiobazze@gmail.com',
 
   // Whether to require proof of email address ownership any time a new user
   // signs up, or when an existing user attempts to change their email address.
-  verifyEmailAddresses: false,
+  verifyEmailAddresses: true,
 
   /**************************************************************************
   *                                                                         *
