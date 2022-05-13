092a56213cd15bc168835f6ad1820bfb2c605ae3
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-05-13 21:11:41 +0100

    set up routes with locals

--- config/routes.js
+++ config/routes.js
@@ -9,73 +9,181 @@
  */
 
 module.exports.routes = {
-
   //  ╦ ╦╔═╗╔╗ ╔═╗╔═╗╔═╗╔═╗╔═╗
   //  ║║║║╣ ╠╩╗╠═╝╠═╣║ ╦║╣ ╚═╗
   //  ╚╩╝╚═╝╚═╝╩  ╩ ╩╚═╝╚═╝╚═╝
-  'GET /':                   { action: 'view-homepage-or-redirect' },
-  'GET /welcome/:unused?':   { action: 'dashboard/view-welcome' },
-  'GET /activatelicense': { action: 'view-activatelicense' },
-  'GET /activate/:licenseKey': {action: 'license/check-if-key-is-valid'},
-  'GET /faq':                { action:   'view-faq' },
-  'GET /legal/terms':        { action:   'legal/view-terms' },
-  'GET /legal/privacy':      { action:   'legal/view-privacy' },
-  'GET /contact':            { action:   'view-contact' },
-  'GET /services': { action: 'view-services' },
-  'GET /bot/login/:username/:machineId': { action: 'talktobotlogin' },
-  'GET /bot/signup/:username/:password/:machineId': { action: 'talktobotsignup' },
-  'GET /revoke/user/:id': { action: 'revoke-user-access' },
+  "GET /": {
+    action: "view-homepage-or-redirect",
+    locals: {
+      pageTitle: "Home Page",
+    },
+  },
+  "GET /welcome/:unused?": {
+    action: "dashboard/view-welcome",
+    locals: {
+      pageTitle: "Dashboard",
+    },
+  },
+  "GET /activatelicense": {
+    action: "view-activatelicense",
+    locals: {
+      pageTitle: "Activate License",
+    },
+  },
+  "GET /activate/:licenseKey": { action: "license/check-if-key-is-valid" },
+  "GET /faq": {
+    action: "view-faq",
+    locals: {
+      pageTitle: "FAQ",
+    },
+  },
+  "GET /legal/terms": {
+    action: "legal/view-terms",
+    locals: {
+      pageTitle: "Terms & Conditions",
+    },
+  },
+  "GET /legal/privacy": {
+    action: "legal/view-privacy",
+    locals: {
+      pageTitle: "Privacy Policy",
+    },
+  },
+  "GET /contact": {
+    action: "view-contact",
+    locals: {
+      pageTitle: "Contact Us",
+    },
+  },
+  "GET /services": {
+    action: "view-services",
+    locals: {
+      page: "Our Services",
+    },
+  },
+  "GET /bot/login/:username/:machineId": { action: "talktobotlogin" },
+  "GET /bot/signup/:username/:password/:machineId": {
+    action: "talktobotsignup",
+  },
+  "GET /revoke/user/:id": { action: "revoke-user-access" },
 
+  "GET /signup": {
+    action: "entrance/view-signup",
+    locals: {
+      pageTitle: "Sign Up",
+    },
+  },
+  "GET /email/confirm": {
+    action: "entrance/confirm-email",
+    locals: {
+      pageTitle: "Confirm Email",
+    },
+  },
+  "GET /email/confirmed": {
+    action: "entrance/view-confirmed-email",
+    locals: {
+      pageTitle: "Email Confirmed",
+    },
+  },
 
-  'GET /signup':             { action: 'entrance/view-signup' },
-  'GET /email/confirm':      { action: 'entrance/confirm-email' },
-  'GET /email/confirmed':    { action: 'entrance/view-confirmed-email' },
+  "GET /login": {
+    action: "entrance/view-login",
+    locals: {
+      pageTitle: "Login",
+    },
+  },
+  "GET /delete/user/:id": { action: "delete-user" },
+  "GET /password/forgot": {
+    action: "entrance/view-forgot-password",
+    locals: {
+      pageTitle: "Forgot Password",
+    },
+  },
+  "GET /password/new": {
+    action: "entrance/view-new-password",
+    locals: {
+      pageTitle: "Set New Password",
+    },
+  },
 
-  'GET /login':              { action: 'entrance/view-login' },
-  'GET /delete/user/:id':              { action: 'delete-user' },
-  'GET /password/forgot':    { action: 'entrance/view-forgot-password' },
-  'GET /password/new':       { action: 'entrance/view-new-password' },
-
-  'GET /account':            { action: 'account/view-account-overview' },
-  'GET /account/password':   { action: 'account/view-edit-password' },
-  'GET /check/expiry':   { action: 'check-expiry' },
-  'GET /account/profile':    { action: 'account/view-edit-profile' },
-  'GET /list-audiobaze-normalusers': { action: 'view-list-audiobaze-normalusers'},
-  'GET /list-users': { action: 'license/list-users'},
-  'POST /activate/licensekey': { action: 'license/activate-license'},
-  'GET /list-keys': { action: 'view-list-keys' },  
-  'GET /view-keys/valid': { action: 'send-list-keys-valid' },  
-  'GET /view-keys/activated': { action: 'list-noactive-licenses' },  
-  'GET /view-keys/unactivated-users': { action: 'list-unactivated-users' },  
-  'GET /generate-keys': { action: 'generate-keys' },  
-  'GET /blog': { action: 'view-blog' },
+  "GET /account": {
+    action: "account/view-account-overview",
+    locals: {
+      pageTitle: "Account Overview",
+    },
+  },
+  "GET /account/password": {
+    action: "account/view-edit-password",
+    locals: {
+      pageTitle: "Change Password",
+    },
+  },
+  "GET /check/expiry": { action: "check-expiry" },
+  "GET /account/profile": {
+    action: "account/view-edit-profile",
+    locals: {
+      pageTitle: "Edit Profile",
+    },
+  },
+  "GET /list-audiobaze-normalusers": {
+    action: "view-list-audiobaze-normalusers",
+    locals: {
+      pageTitle: "Audiobaze Users",
+    },
+  },
+  "GET /list-users": { action: "license/list-users" },
+  "POST /activate/licensekey": { action: "license/activate-license" },
+  "GET /list-keys": {
+    action: "view-list-keys",
+    locals: {
+      pageTitle: "List Valid License Keys",
+    },
+  },
+  "GET /view-keys/valid": { action: "send-list-keys-valid" },
+  "GET /view-keys/activated": { action: "list-noactive-licenses" },
+  "GET /view-keys/unactivated-users": { action: "list-unactivated-users" },
+  "GET /generate-keys": { action: "generate-keys" },
+  "GET /blog": { action: "view-blog" },
   //  ╔╦╗╦╔═╗╔═╗  ╦═╗╔═╗╔╦╗╦╦═╗╔═╗╔═╗╔╦╗╔═╗   ┬   ╔╦╗╔═╗╦ ╦╔╗╔╦  ╔═╗╔═╗╔╦╗╔═╗
   //  ║║║║╚═╗║    ╠╦╝║╣  ║║║╠╦╝║╣ ║   ║ ╚═╗  ┌┼─   ║║║ ║║║║║║║║  ║ ║╠═╣ ║║╚═╗
   //  ╩ ╩╩╚═╝╚═╝  ╩╚═╚═╝═╩╝╩╩╚═╚═╝╚═╝ ╩ ╚═╝  └┘   ═╩╝╚═╝╚╩╝╝╚╝╩═╝╚═╝╩ ╩═╩╝╚═╝
-  '/terms':                   '/legal/terms',
-  '/logout':                  '/api/v1/account/logout',
-
+  "/terms": "/legal/terms",
+  "/logout": "/api/v1/account/logout",
 
   //  ╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═╔═╗
   //  ║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗╚═╗
   //  ╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝
   // …
 
-
   //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
   //  ╠═╣╠═╝║  ║╣ ║║║ ║║╠═╝║ ║║║║║ ║ ╚═╗
   //  ╩ ╩╩  ╩  ╚═╝╝╚╝═╩╝╩  ╚═╝╩╝╚╝ ╩ ╚═╝
   // Note that, in this app, these API endpoints may be accessed using the `Cloud.*()` methods
   // from the Parasails library, or by using those method names as the `action` in <ajax-form>.
-  '/api/v1/account/logout':                           { action: 'account/logout' },
-  'PUT   /api/v1/account/update-password':            { action: 'account/update-password' },
-  'PUT   /api/v1/account/update-profile':             { action: 'account/update-profile' },
-  'PUT   /api/v1/account/update-billing-card':        { action: 'account/update-billing-card' },
-  'PUT   /api/v1/entrance/login':                        { action: 'entrance/login' },
-  'POST  /api/v1/entrance/signup':                       { action: 'entrance/signup' },
-  'POST  /api/v1/entrance/send-password-recovery-email': { action: 'entrance/send-password-recovery-email' },
-  'POST  /api/v1/entrance/update-password-and-login':    { action: 'entrance/update-password-and-login' },
-  'POST  /api/v1/deliver-contact-form-message':          { action: 'deliver-contact-form-message' },
-  'POST  /api/v1/observe-my-session':                 { action: 'observe-my-session', hasSocketFeatures: true },
-
+  "/api/v1/account/logout": { action: "account/logout" },
+  "PUT   /api/v1/account/update-password": {
+    action: "account/update-password",
+  },
+  "PUT   /api/v1/account/update-profile": { action: "account/update-profile" },
+  "PUT   /api/v1/account/update-billing-card": {
+    action: "account/update-billing-card",
+  },
+  "PUT   /api/v1/entrance/login": { action: "entrance/login" },
+  "POST  /api/v1/entrance/signup": { action: "entrance/signup" },
+  "POST  /api/v1/entrance/send-password-recovery-email": {
+    action: "entrance/send-password-recovery-email",
+  },
+  "POST  /api/v1/entrance/update-password-and-login": {
+    action: "entrance/update-password-and-login",
+  },
+  "POST  /api/v1/deliver-contact-form-message": {
+    action: "deliver-contact-form-message",
+  },
+  "POST  /api/v1/observe-my-session": {
+    action: "observe-my-session",
+    hasSocketFeatures: true,
+  },
+  "GET /edit-user-profile/:profileId": {
+    action: "edit-user-profile",
+  },
 };
