7e97fc4307f4641b3b40feb0c32cf81a8b5214d0
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-05-16 09:44:43 +0100

    ui updates

--- assets/js/pages/services.page.js
+++ assets/js/pages/services.page.js
@@ -1,4 +1,4 @@
-parasails.registerPage('services', {
+parasails.registerPage("services", {
   //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
@@ -9,11 +9,64 @@ parasails.registerPage('services', {
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
-  beforeMount: function() {
+  beforeMount: function () {
     //…
   },
-  mounted: async function() {
-    //…
+  mounted: async function () {
+    // Create Modal Component
+    // const modal = document.createElement("div");
+    // Modal Container
+    // const container = document.getElementById("modal_container");
+
+    // Buy Streams Modal
+    // const streamsModal = document.createElement("div");
+    // const buyStreamsBtn = document.getElementById("buy_streams_btn");
+
+    // streamsModal.innerHTML = `
+    //   <div class="modal fade" id="streamsModal" tabindex="-1" aria-labelledby="streamsModalLabel" aria-hidden="true">
+    //     <div class="modal-dialog">
+    //       <div class="modal-content">
+    //         <div class="modal-header">
+    //           <h5 class="modal-title" id="streamsModalLabel">Buy Music Streams</h5>
+    //           <button type="button" class="close closebuystream" data-dismiss="modal" aria-label="Close">
+    //             <span aria-hidden="true">&times;</span>
+    //           </button>
+    //         </div>
+    //         <div class="modal-body">
+    //           <div>
+    //             You are about to make payment of 70 USDT to purchase 100k streams for one of the following platforms. <br/>
+    //             SoundCloud/Spotify/Audiomack <br />
+    //             After payment you'll be redirected to the admin to activate your account.
+    //           </div>
+    //         </div>
+    //         <div class="modal-footer">
+    //           <button type="button" class="btn btn-secondary closebuystream" data-dismiss="modal">Close</button>
+    //           <button type="button" class="btn btn-warning">Confirm Purchase</button>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // `;
+    // Show Buy Stream Modal
+    // buyStreamsBtn.addEventListener("click", () => {
+    //   container.appendChild(streamsModal);
+    //   $("#streamsModal").modal("show");
+      
+    //   const closeStreambtn = $(".closebuystream");
+    //   closeStreambtn.click(() => {
+    //     $("#streamsModal").modal("hide");
+    //     clearModal(streamsModal);
+    //   });
+    // });
+
+    // Remove Modal from Container Element
+    // function clearModal(modal) {
+    //   modal.parentNode.removeChild(modal);
+    // }
+
+    // Single Use VPS
+    // const singleUseVPS = document.createElement("div");
+
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
@@ -21,5 +74,5 @@ parasails.registerPage('services', {
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
     //…
-  }
+  },
 });
